bcc065d2abd264b62e216d726af48a68f8fc3ae2
ReyvanMacross <88920121+ReyvanMacross@users.noreply.github.com>
2023-11-17 14:31:11 +0700

    Weekly Coding hope now win

--- advance1.py
+++ advance1.py
@@ -1,15 +1,29 @@
-def squared_sum(num):
-    return sum(int(digit) ** 2 for digit in str(num))
+def count_paths(n, grid):
+    # Matriks untuk menyimpan jumlah jalur optimal ke setiap sel
+    dp = [[0] * n for _ in range(n)]
 
-def is_happy_number(num):
-    seen = set()
-    while num != 1 and num not in seen:
-        seen.add(num)
-        num = squared_sum(num)
-    return num == 1
+    # Inisialisasi dp[0][0] berdasarkan kondisi awal
+    dp[0][0] = 1 if grid[0][0] == '.' else 0
 
-num = int(input("Masukkan Angka : "))
-if is_happy_number(num):
-    print(num, "Adalah Happy Number / true")
-else:
-    print(num, "Bukan Happy Number / false")
+    # Mengisi dp untuk setiap sel berdasarkan kondisi grid
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == '*':
+                dp[i][j] = 0  # Tidak bisa melewati sel dengan jebakan
+            else:
+                # Menambahkan jumlah jalur dari sel di atas dan sel di kiri
+                if i > 0:
+                    dp[i][j] += dp[i - 1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j - 1]
+
+    # Mengembalikan jumlah jalur optimal ke kotak kanan bawah
+    return dp[n - 1][n - 1]
+
+# Membaca input
+n = int(input("Input: "))
+grid = [input()[:n] for _ in range(n)]  # Memastikan setiap baris memiliki panjang n
+
+# Menghitung dan mencetak output
+result = count_paths(n, grid)
+print("Output:", result)
